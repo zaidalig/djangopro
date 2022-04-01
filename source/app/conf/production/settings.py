461332303b4651eb4fced1a30c1480ba51bda86e
zaidalig <me.mzaidali@gmail.com>
2022-04-01 16:31:46 +0500

    with login and logout and registration

--- source/app/conf/production/settings.py
+++ source/app/conf/production/settings.py
@@ -75,8 +75,12 @@ EMAIL_USE_SSL = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.path.join(BASE_DIR, 'new'),
+        'USER': 'root',
+        'PASSWORD' : '',
+        'HOST':'localhost',
+        'PORT': '3306',
     }
 }
 
